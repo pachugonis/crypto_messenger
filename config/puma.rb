b700bcded892622115176221f5b57022d6b1ce7f
Igor <igor@MacBook-Air-Igor.local>
2026-01-28 16:08:48 +0300

    Configure Puma to use Unix socket in production

--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,12 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# In production with Nginx, use Unix socket instead of TCP port
+if ENV["RAILS_ENV"] == "production"
+  bind "unix:///var/www/vorthex/crypto_messenger/tmp/sockets/puma.sock"
+else
+  port ENV.fetch("PORT", 3000)
+end
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
